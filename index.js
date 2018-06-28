3079e15521d042cde7366a7907818c7bdd82cfef
Tim Shaker <tmshkr@users.noreply.github.com>
2018-06-28 02:39:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+add(a, b){
+    
+}
+
+subtract(a, b){
+    
+}
+
+multiply(a, b){
+    
+}
+
+divide(a, b){
+    
+}
+
+inc(n){
+    
+}
+
+dec(n){
+    
+}
+
+makeInt(n){
+    
+}
+
+preserveDecimal(n){
+    
+}
